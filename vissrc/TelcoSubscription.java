4d68d50f22031bae65c5322acb27a4b67f75bdfd
Frinz Hughwie Bautista <frinzbrothers@gmail.com>
2026-03-03 11:37:25 +0800

    Delete vissrc directory

--- vissrc/TelcoSubscription.java
+++
@@ -1,9 +0,0 @@
-public interface TelcoSubscription {
-
-    String accept(UsagePromo promo, double promoPrice);
-    String accept(UnliCallTextOffer offer, boolean unliCallText);
-    
-    String getTelcoName();
-    double getPromoPrice();
-    boolean getUnliCallText();
-}
